50bbf6ea9c9d20ef4fcac7a96444355a315fbc8e
Blaze Fugina <blaze.fugina@gmail.com>
2017-10-27 08:33:40 -0500

    Most of base mode working except for names appearing.

---
+++ client.js
@@ -0,0 +1,23 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('GuessWhoController', function(){
+    var guess = this;
+   
+guess.addPeople = function (person){
+     var newPeople = {name: guess.nameIn, github: guess.githubIn};
+     people.push(newPeople)
+     console.log(newPeople);
+ };
+
+guess.displayName = function(person){
+    
+   var personMessage = ("This is" + person.name);
+};
+ 
+var people = [{name: 'Sarah', github: 'SarahHarrington'}, 
+{name: 'Holly', github: 'hollytuhake'}, 
+{name: 'Alex', github: 'alexanderbliss'}];
+
+guess.people = people;
+console.log(guess.people);
+});
